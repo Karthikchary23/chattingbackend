8a7bc30e25b7074826d4bec0e03f9552879902a7
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-04-07 14:46:45 +0530

    front

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import { Server } from "socket.io";
 import http from "http";
 import nodemailer from "nodemailer";
 
+
 dotenv.config();
 
 const app = express();
@@ -19,7 +20,7 @@ const PORT = 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://chattingbackend-79ur.onrender.com",
   credentials: true,
 }));
 app.use(cookieParser());
